ec352c9a57d7562755afcdf4ec65ad17ccd0d9f7
Mitul Kalariya <mitul.k@simformsolutions.com>
2023-05-12 15:44:14 +0530

    Day: 6 added full_text search and search page

--- AMAZE/products/urls.py
+++ AMAZE/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path("home/",views.home_view.as_view(), name="home")
+    path("home/",views.home_view.as_view(), name="home"),
+    path("search/",views.search_view.as_view(), name="search")
 ]
--- AMAZE/products/views.py
+++ AMAZE/products/views.py
@@ -1,7 +1,9 @@
+import time
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,View
 from products.models import Products
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 # Create your views here.
 
 # class home_view(ListView):
@@ -13,3 +15,27 @@ class home_view(LoginRequiredMixin,ListView):
     template_name = 'Product/home.html'
     context_object_name = "products"
     paginate_by = 12
+
+
+class search_view(LoginRequiredMixin,ListView):
+    model = Products
+    template_name = 'Product/search.html'
+    context_object_name = "products"
+    paginate_by = 12
+
+
+    def post(self,request):
+        self.object_list = self.get_queryset()
+        return render(request,'Product/search.html',{'products':self.object_list})
+    
+    def get_queryset(self):
+        qs = Products.objects.all()
+        if self.request.POST:
+            query = self.request.POST.get('query')
+            # qs = Products.objects.annotate(rank=SearchRank(SearchVector('name'), SearchQuery(query))).order_by('-rank')
+            start_time = time.perf_counter()
+            qs =  Products.objects.annotate(search=SearchVector('name','main_category','sub_category' ),).filter(search=SearchQuery(query))
+            data = {"queryset":qs,"time":str(time.perf_counter()-start_time), "query":query }
+            return data
+        else:
+            return []
